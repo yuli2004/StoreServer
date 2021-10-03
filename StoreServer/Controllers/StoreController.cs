f294a47d41ff41b291f682da890bdd0b4e96f31f
User <User@LAB2-8>
2021-10-03 09:35:20 +0300

    added session extension and librarys

---
+++ StoreServer/Controllers/StoreController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using StoreServerBL.Models;
+using System.IO;
+
+namespace StoreServer.Controllers
+{
+    [Route("StoreAPI")]
+    [ApiController]
+    public class StoreController : ControllerBase
+    {
+        #region Add connection to the db context using dependency injection
+        StoreDBContext context;
+        public StoreController(StoreDBContext context)
+        {
+            this.context = context;
+        }
+        #endregion
+    }
+}
