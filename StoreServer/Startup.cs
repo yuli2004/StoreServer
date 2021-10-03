cb25217bfbee1e788df9df757e1e6ef8771fcece
User <User@LAB2-8>
2021-10-03 08:50:49 +0300

    1

--- StoreServer/Startup.cs
+++ StoreServer/Startup.cs
@@ -18,10 +18,42 @@ namespace StoreServer
 {
     public class Startup
     {
+        #region Add Constructor with Configuration using Dependency Injection
+        //This constructor need to be added to get an instance of configuration file (appsettings.json)
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        #endregion
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
+            //Add Controllers and set the Json Serializer to handle loop referencing
+            services.AddControllers().AddJsonOptions(o => o.JsonSerializerOptions
+                        .ReferenceHandler = ReferenceHandler.Preserve);
+            //The following two commands set the Session state to work!
+            services.AddDistributedMemoryCache();
+
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(180);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+
+            //The following set the connection string to the DB and DB context!
+            #region Add DB Context Support
+            string connectionString = this.Configuration.GetConnectionString("ContactsDB");
+
+            services.AddDbContext<StoreDBContext>(options => options
+                                                                .UseSqlServer(connectionString));
+            //.UseLazyLoadingProxies());
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,14 +64,13 @@ namespace StoreServer
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles(); //Added to have the wwwroot folder and server to accept calls to static files
             app.UseRouting();
+            app.UseSession(); //Added to tell the server to use sessions!
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
