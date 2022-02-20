868d7a915a40b1b03ced985063022bab67c18a20
User <User@LAB2-8>
2022-02-20 10:30:32 +0200

    ujrytjtr

--- StoreServer/DTO/LookupTables.cs
+++ StoreServer/DTO/LookupTables.cs
@@ -9,8 +9,8 @@ namespace StoreServer.DTO
     public class LookupTables
     {
         public List<Color> Colors { get; set; }
-        public List<Material> SurfaceMaterials { get; set; }
-        public List<Material> PaintMaterials { get; set; }
+        public List<SurfaceMaterial> SurfaceMaterials { get; set; }
+        public List<PaintMaterial> PaintMaterials { get; set; }
         public List<Style> Styles { get; set; }
         public  List<Product> AllProducts { get; set; }
     }
